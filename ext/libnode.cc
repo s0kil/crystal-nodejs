b713c8aa0ef3b7e8fcb7bdf3cceb5950265b62c3
fukaoi <goodakira7@gmail.com>
2019-06-05 19:06:20 +0900

    WIP

--- ext/libnode.cc
+++ ext/libnode.cc
@@ -2,8 +2,14 @@
 
 int node::Start(int argc, char* argv[]);
 
-int main(int argc, char* argv[])
+int main()
 {
-  node::Start(argc, argv);
+  char argv0[] = "crystal_nodejs";
+  char argv1[] = "-e";
+  char argv2[] = "console.log(1)";
+  char *argv[] = {argv0, argv1, argv2, NULL};
+  node::Start(3, argv);
   return 0;
 }
+
+
