fbb9f7f724564ce2fa5ce8963999c700226eb9ca
fukaoi <goodakira7@gmail.com>
2019-06-06 15:10:58 +0900

    modified

--- ext/libnode.cc
+++ ext/libnode.cc
@@ -2,13 +2,27 @@
 
 int node::Start(int argc, char* argv[]);
 
+// int main()
+// {
+  // char argv0[] = "test test test program";
+  // char argv1[] = "-e";
+  // char argv2[] = "console.log(1)";
+  // char *argv[] = {argv0, argv1, argv2};
+  // node::Start(3, argv);
+
+  // return 0;
+// }
+
 int main()
 {
-  char argv0[] = "crystal_nodejs";
+  // char argv0[] = "test_program";
+  // char argv0[] = "crystal_node";
+  char argv0[] = "crystal_node";
   char argv1[] = "-e";
-  char argv2[] = "console.log(1)";
+  char argv2[] = "console.log('aaaa');class A {a(n){console.log(n);}}; new A().a(111111);";
   char *argv[] = {argv0, argv1, argv2, NULL};
   node::Start(3, argv);
+
   return 0;
 }
 
