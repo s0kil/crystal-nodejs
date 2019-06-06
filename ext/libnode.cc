8c39eb548227c5fe4ea270b0fc82c4be76266b28
fukaoi <goodakira7@gmail.com>
2019-06-06 15:41:57 +0900

    modified

--- ext/libnode.cc
+++ ext/libnode.cc
@@ -4,6 +4,7 @@ int node::Start(int argc, char* argv[]);
 
 int main()
 {
+
   char argv0[] = "crystal_node";
   char argv1[] = "-e";
   char argv2[] = "console.log('aaaa');class A {a(n){console.log(n);}}; new A().a(111111);";
